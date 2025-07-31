031879cf7e20028646286398b26e36967e4a6c57
jiebinzhou98 <benzhou98@outlook.com>
2025-07-31 15:40:23 -0400

    feat: simple setup for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <h1>hello world</h1>
-    </div>
-  );
+    <main className="p-4 flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Personal Tracker</h1>
+
+      <div className="flex flex-col gap-2">
+      <Button variant="outline" className="justify-start">Log Coffee</Button>
+      <Button variant="outline" className="justify-start">Log Exercise</Button>
+      <Button variant="outline" className="justify-start">Log Job Application</Button>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Coffees</p>
+          <p>Exercise</p>
+          <p>Job Application</p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Calendar</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Calendar</p>
+        </CardContent>
+      </Card>
+    </main>
+  )
 }
